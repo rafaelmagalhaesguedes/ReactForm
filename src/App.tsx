9439265d2295acb7b1217804b5e7a4012775ff24
Rafael <rafaelmagalhaes1@live.com>
2023-08-28 12:23:23 -0300

    style: adjust margins component Footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,22 @@ import Register from './pages/Register';
 function App() {
 
   return (
-    <div className="container">
-      
-      <Header />
-      
-      <main>
-        <section>
-          <Routes>
-            <Route path="/" element={ <Home /> } />
-            <Route path="/login" element={ <Login />} />
-            <Route path="/register" element={ <Register /> } />
-          </Routes>
-        </section>
-      </main>
+    <>
+      <div className="container">
+        <Header />
+        <main>
+          <section>
+            <Routes>
+              <Route path="/" element={ <Home /> } />
+              <Route path="/login" element={ <Login />} />
+              <Route path="/register" element={ <Register /> } />
+            </Routes>
+          </section>
+        </main>
+      </div>
 
       <Footer />
-  
-    </div>
+    </>
   )
 }
 
